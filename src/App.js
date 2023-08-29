f3dbb50dd72989bd9c241d032660bd0682f1d893
Priscilla Son <priscilla.lee.son@gmail.com>
2023-08-28 23:09:06 -0700

    Style App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Excerpt } from "./MobyDick";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="m-2">Moby Dick</h1>
+    <div className="App m-10">
+      <div className="flex justify-center">
+        <h1 className="m-2 text-lg font-lora">Moby Dick by Herman Melville</h1>
+      </div>
+      <Excerpt />
     </div>
   );
 }
