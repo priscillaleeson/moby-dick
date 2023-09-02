4aee19d60a647331291ec95a7a10c10f5118e156
Priscilla Son <priscilla.lee.son@gmail.com>
2023-09-01 17:25:42 -0700

    Rewrite API fetch function

--- src/webster-dictionary-api.js
+++ src/webster-dictionary-api.js
@@ -1,12 +1,32 @@
 import axios from "axios";
 
-const getApiObject = async (text) => {
-  console.log(text);
+const getApiObject = async (word) => {
+  console.log("word: ", word);
   const response = await axios.get(
-    `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${text}?key=9e06ee89-3af3-4280-b0ab-a6f4f6b862d0`
+    `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=9e06ee89-3af3-4280-b0ab-a6f4f6b862d0`
   );
-  console.log(response);
-  return response;
+
+  console.log("Response", {
+    response,
+    data: response.data,
+    typeOfData: typeof response.data,
+  });
+
+  // const isArray = typeof response.data === Array;
+
+  return response.data.map((usage, i) => {
+    console.log("usage", usage);
+    return usage;
+
+    // return usage.shortdef;
+  });
+
+  // if (isArray) {
+  //   return response.data.map((usage, i) => {
+  //     return usage.shortdef;
+  //   });
+  // }
+  // return [];
 };
 
 export default getApiObject;
