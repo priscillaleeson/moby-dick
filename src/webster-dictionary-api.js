2c56a3348ef1aa3153de481b495f6af1cda261e2
Priscilla Son <priscilla.lee.son@gmail.com>
2023-09-05 16:56:24 -0700

    Comment out try and catch statements

--- src/webster-dictionary-api.js
+++ src/webster-dictionary-api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const getApiObject = async (word) => {
   console.log("word: ", word);
+
+  // try {
   const response = await axios.get(
     `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=9e06ee89-3af3-4280-b0ab-a6f4f6b862d0`
   );
@@ -12,21 +14,22 @@ const getApiObject = async (word) => {
     typeOfData: typeof response.data,
   });
 
+  // if (!response.data || !response.data.length) {
+  //   throw new Error("No definition found for this word.");
+  // }
+
   // const isArray = typeof response.data === Array;
 
   return response.data.map((usage, i) => {
-    console.log("usage", usage);
+    // console.log("usage", usage);
     return usage;
 
     // return usage.shortdef;
   });
-
-  // if (isArray) {
-  //   return response.data.map((usage, i) => {
-  //     return usage.shortdef;
-  //   });
+  // } catch (error) {
+  //   console.error("Error fetching definition", error.message);
+  //   throw error;
   // }
-  // return [];
 };
 
 export default getApiObject;
