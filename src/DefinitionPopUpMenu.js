3df5bf6a586421c6efb917921fd8beb917e4a881
Priscilla Son <priscilla.lee.son@gmail.com>
2023-09-06 21:07:03 -0700

    Add handledelete function

--- src/DefinitionPopUpMenu.js
+++ src/DefinitionPopUpMenu.js
@@ -1,4 +1,12 @@
-export function DefinitionPopUpMenu({ selectedWord }) {
+export function DefinitionPopUpMenu({
+  setSelectedWord,
+  selectedWord,
+  initialState,
+}) {
+  const handleDelete = () => {
+    setSelectedWord(initialState);
+  };
+
   console.log("selectedWord", selectedWord);
   console.log("this is selectedWord.position:", selectedWord.position);
   return (
@@ -10,6 +18,12 @@ export function DefinitionPopUpMenu({ selectedWord }) {
       className=" p-5 absolute rounded-sm z-10 text-black bg-indigo-200"
     >
       <div>
+        <div
+          onClick={handleDelete}
+          className="absolute top-1 right-2 text-white font-sans text-lg cursor-pointer hover:text-violet-600 active:font-semibold"
+        >
+          x
+        </div>
         <div className="font-bold lowercase">{selectedWord.word}</div>
         <div className="italic mb-2">{selectedWord.usage}</div>
         <div>
